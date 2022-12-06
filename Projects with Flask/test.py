62357557eba8a75ee38ba7375602b73fc6e02263
Velin-Todorov <velintodorov@yahoo.com>
2022-12-06 22:55:38 +0100

    Created a navigation bar, defined proper views css

--- Projects with Flask/test.py
+++ Projects with Flask/test.py
@@ -1,28 +1,60 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from login_form import NameForm
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'hello'
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def landing_page():
+    """This view is concerned with the initial screen"""
+    return render_template('landing_page.html')
 
-@app.route('/catalog')
-def catalog():
-    return '<h1> Welcome to our online catalog!</h1>'
+@app.route('/register')
+def register():
+    """This view handles registration"""
+    return render_template('register.html')
 
-@app.route('/user')
-@app.route('/user/<name>')
-def user_profile(name=None):
+@app.route('/login')
+def login():
+    """This view handles login form"""
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('login.html', form=form)
+
+
+@app.route('/all-items')
+def home():
+    """This is  the homepage that will display all the "cards" """
+    return render_template('home.html')
+
+@app.route('/bank_cards')
+def banking(name=None):
+    """This view will show bank cards "cards" """
     return render_template('user.html', name=name)
 
-@app.route('/shop')
-def shop():
+@app.route('/websites')
+def websites():
+    """This view will show the password "cards" for websites"""
     return '<p>Welcome to our online shop</p>'
+
+@app.route('/other')
+def other():
+    """This view is concerned with all other passwords that are not labeled"""
+    return '<h1>Other</h1>'
+
+
+
+
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
