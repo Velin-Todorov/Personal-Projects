e5ed48e5b3c36dec38a9f8f672b35a3255c1034c
Velin-Todorov <velintodorov@yahoo.com>
2022-12-29 11:24:27 +0100

    DB Models done + small changes in names

--- Projects with Flask/DailyCheck/forms.py
+++ Projects with Flask/DailyCheck/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField, Email
 from wtforms.validators import Email, InputRequired, Length
 
 
-class Login_form(FlaskForm):
+class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[Email(message= "Invalid email")])
     password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
 
 
-class Register_form(FlaskForm):
+class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=5, max=20, message='Username must be between 5 and 20 characters long')])
     email = EmailField('Email', validators=[Email(message='Invalid email', check_deliverability=True)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=64, message='Password must be between 8 and 64 characters')])
