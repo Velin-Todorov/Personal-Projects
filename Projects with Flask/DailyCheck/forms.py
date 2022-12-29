f4cf8d6949c12372ca91cf66be370d9b76267b14
Velin-Todorov <velintodorov@yahoo.com>
2022-12-29 11:12:17 +0100

    Register form done

--- Projects with Flask/DailyCheck/forms.py
+++ Projects with Flask/DailyCheck/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Regexp
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, EmailField
+from wtforms.validators import Email, InputRequired, Length
 
 
 class Login_form(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Regexp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[Email(message= "Invalid email")])
+    password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+
+class Register_form(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=5, max=20, message='Username must be between 5 and 20 characters long')])
+    email = EmailField('Email', validators=[Email(message='Invalid email', check_deliverability=True)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=64, message='Password must be between 8 and 64 characters')])
+    repeat_password = PasswordField('Repeat Password', validators=[InputRequired()])
+
