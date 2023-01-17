9f63ddc07f4f6d8f664ca5034cda296b6b4e1577
Velin-Todorov <velintodorov@yahoo.com>
2023-01-17 20:59:52 +0100

    async

--- Projects with Flask/DailyCheck/static/news.js
+++ Projects with Flask/DailyCheck/static/news.js
@@ -5,5 +5,17 @@ import { newsApiKey } from './api_key.js';
 let submit = document.querySelector('#submit')
 let apiKey = weatherApiKey()
 
-submit.addEventListener('click', getLocationKey)
+submit.addEventListener('click', getArticles)
 
+async function getArticles(){
+
+    const searchTerm = document.querySelector('#searchBar').value
+    const url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${newsApiKey}`
+
+    const request = await fetch(url)
+
+    const response = await request.json()
+
+    console.log(response)
+
+}
