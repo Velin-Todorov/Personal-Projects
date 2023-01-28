9477187445eabca1d02587389dbb7b676f059bc2
Velin-Todorov <velintodorov@yahoo.com>
2023-01-28 16:58:22 +0100

    changed the template of news

--- Projects with Flask/DailyCheck/static/newsTemplate.js
+++ Projects with Flask/DailyCheck/static/newsTemplate.js
@@ -6,31 +6,30 @@ export function renderArticles(data, totalResults){
     let el = html`
         <h1> ${totalResults} result/s found</h1>
 
-            <br>
+        <br>
+        <div class="row m-5">
+            ${data.map((item) => html`
+                <div class="col-md-5 offset-md-3" style="align-items: center">
+                    <div class="card border-dark mb-5 card text-dark">
+                        <img src="${item.urlToImage}" style="width:720px; height:720px">
+                            <div class="card-body" style="height:auto; width:auto;">
+                                                                    
+                                <h5 class="card-title" style="font-size: 30px; display: inline;"> 
+                                    <a href="${item.url}" target="_blank" rel="noopener noreferrer" >${item.title}</a>
+                                    
+                                </h5>
 
-            <div class="row m-5">
-                ${repeat(data, (item) => html`
-                    <div class="col">
-                        <div class="card border-dark mb-5 card text-dark">
-                            <img src="${item.urlToImage}" style="max-width:310px; max-height:300px">
-                                <div class="card-body" style="height:auto; width:auto;">
-                                                                        
-                                    <h5 class="card-title" style="font-size: 30px; display: inline;"> 
-                                        <a href="${item.url}" target="_blank" rel="noopener noreferrer" >${item.title}</a>
-                                        
-                                    </h5>
+                                <p class="card-text" style="font-size: 20px">
+                                    By: ${item.author}
+                                </p>
 
-                                    <p class="card-text" style="font-size: 20px">
-                                        By: ${item.author}
-                                    </p>
-
-                                    <p class="card-text" style="font-size: 20px">
-                                        Published on: ${new Date(item.publishedAt).toLocaleDateString('en-GB')}
-                                    </p>
-                                </div>
-                        </div>
-                    </div>`)}
-            </div>
+                                <p class="card-text" style="font-size: 20px">
+                                    Published on: ${new Date(item.publishedAt).toLocaleDateString('en-GB')}
+                                </p>
+                            </div>
+                    </div>
+                </div>`)}
+        </div>
 
 
     `
