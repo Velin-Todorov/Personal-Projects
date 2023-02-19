06d551c14d9059fa92a50aab2a43c27ee1700e8b
Velin-Todorov <velintodorov@yahoo.com>
2023-02-19 19:39:08 +0100

    Started building main functionality of the board

--- Projects with Flask/Chat Rooms/JS/main.js
+++ Projects with Flask/Chat Rooms/JS/main.js
@@ -1,6 +1,55 @@
-import { createBoard, playMove } from "./connect4.js"; 
+import { createBoard, playMove } from "./connect4.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     const board = document.querySelector('.board');
     createBoard(board)
+
+    const websocket = new WebSocket('ws://localhost:8001/');
+    receiveMoves(board, websocket)
+    sendMoves(board, websocket)
 })
+
+function sendMoves(board, websocket) {
+    board.addEventListener('click', (ev) => {
+        const column = ev.target.dataset.column
+
+        if (column == undefined) {
+            return;
+        }
+
+        const event = {
+            type: "play",
+            column: parseInt(column, 10)
+        };
+        websocket.send(JSON.stringify(event))
+    })
+}
+
+function showMessage(message) {
+    window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+    websocket.addEventListener('message', ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+
+            case "play":
+                playMove(board, event.player, event.column, event.row);
+                break;
+
+            case "win":
+                showMessage(`Player ${event.player} wins`);
+                websocket.close(1000);
+                break;
+
+            case "error":
+                console.log('I am here')
+                showMessage(event.message);
+                break;
+
+            default:
+                throw new Error(`Unsupported event type: ${event.type}.`);
+        }
+    });
+}
---
+++ Projects with Flask/Chat Rooms/Python files/app.py
@@ -0,0 +1,54 @@
+import asyncio
+import websockets
+import json
+from connect4 import PLAYER1, PLAYER2, Connect4
+
+
+async def handler(websocket):
+    game = Connect4()
+
+    async for message in websocket:
+        current_player = game.last_player
+        print(message)
+        parsed_message = json.loads(message)
+        column = parsed_message['column']
+
+        try:
+            row = game.play(current_player, int(column))
+
+            event = {
+                'type': 'play',
+                'player': current_player,
+                'column': column,
+                'row': row
+            }
+            print(game.last_player)
+            if game.last_player_won:
+                event = {
+                    'type': 'win',
+                    'player': current_player
+                }                
+                await websocket.send(json.dumps(event))
+         
+            await websocket.send(json.dumps(event))
+
+        except RuntimeError as e:
+            print(e)
+            print('Runtime Error happened')
+            event = {
+                'type': 'error'
+            }
+            await websocket.send(json.dumps(event))
+#        current_player = PLAYER2
+
+async def main():
+    """This is a coroutine that starts a websocket server."""
+    async with websockets.serve(handler, '', 8001):
+        # handler - a coroutine that manages the connection when a client connects or closes the connection.
+        # '' - defines the network interface the network interfaces where the server can be reached. We listen on all interfaces, so that devices on the same local network can connect
+        # 8001 defines the port
+        await asyncio.Future()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
