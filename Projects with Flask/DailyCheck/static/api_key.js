203f15f12e5cff68e8b55a50cfae3e679fdd127a
Velin-Todorov <96660814+Velin-Todorov@users.noreply.github.com>
2023-02-20 22:09:18 +0100

    Update api_key.js

--- Projects with Flask/DailyCheck/static/api_key.js
+++ Projects with Flask/DailyCheck/static/api_key.js
@@ -1,7 +1,1 @@
-export function weatherApiKey(){
-    return 'RXyswNaQZXBYHzRusrcVyMBqbE7OG1ao'
-}
-
-export function newsApiKey(){
-    return 'c8f09cae6ba04a9286891299031dcfe2'
-}
+I have hidden those
