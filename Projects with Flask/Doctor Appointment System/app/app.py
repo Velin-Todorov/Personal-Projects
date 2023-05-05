a8b8df226d062772e8f8d7d38a9b53240dbf7e26
Velin-Todorov <velintodorov@yahoo.com>
2023-05-05 22:04:22 +0200

    Added class-based views

--- Projects with Flask/Doctor Appointment System/app/app.py
+++ Projects with Flask/Doctor Appointment System/app/app.py
@@ -1,7 +1,6 @@
 # All the views are here
 from flask import Flask, render_template, redirect
 from app import create_app
-from patient_views.views import patients, Views
 
 app = create_app()
 # app.register_blueprint(patients)
--- Projects with Flask/Doctor Appointment System/app/patient_views/views.py
+++ Projects with Flask/Doctor Appointment System/app/patient_views/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, typing as ft, render_template
-from flask.views import View, MethodView
+from flask.views import View
 
 
 patients = Blueprint(
@@ -10,23 +10,23 @@ patients = Blueprint(
     url_prefix="/patients"
 )
 
-class Views(View):
-    def __init__(self, template):
-        self.template = template
-
-
+class HomeView(View):
     def dispatch_request(self):
-        return render_template(self.template)
+        return render_template('')
+
+class LoginView(View):
+    def dispatch_request(self):
+        return render_template('login_page.html')
     
-login_view_func = Views.as_view('login', template='login_page.html')
-register_view_func = Views.as_view('register', template='register_page.html')
+# login_view_func = Views.as_view('login', template='login_page.html')
+# register_view_func = Views.as_view('register', template='register_page.html')
 
 patients.add_url_rule(
     '/login/', 
-    view_func=login_view_func
+    view_func=LoginView.as_view('login')
 )
 
-patients.add_url_rule(
-    '/register/',
-    view_fund=register_view_func
-)
+# patients.add_url_rule(
+#     '/register/',
+#     view_fund=register_view_func
+# )
