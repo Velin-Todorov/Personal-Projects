4d2263a757f289333fe248364b25a6bcedae5010
Velin-Todorov <velintodorov@yahoo.com>
2023-05-19 14:14:22 +0200

    Changed the admin html

--- Projects with Django/polling site/mysite/polls/admin.py
+++ Projects with Django/polling site/mysite/polls/admin.py
@@ -1,5 +1,21 @@
+from .models import Question, Choice
 from django.contrib import admin
-from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']})
+    ]
+
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- Projects with Django/polling site/mysite/polls/models.py
+++ Projects with Django/polling site/mysite/polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import datetime
-
+from django.contrib import admin
 from django.utils import timezone
 
 # Create your models here.
@@ -8,6 +8,12 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?'
+    )
 
     def was_published_recently(self):
         now = timezone.now()
@@ -18,4 +24,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     
