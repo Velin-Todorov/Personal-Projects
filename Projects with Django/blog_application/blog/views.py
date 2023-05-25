3d7185ff21665f5bb68dfebc892d517a142caf0a
Velin-Todorov <velintodorov@yahoo.com>
2023-05-25 17:59:44 +0200

    Started working on email form for sending posts

--- Projects with Django/blog_application/blog/views.py
+++ Projects with Django/blog_application/blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 
 # Create your views here.
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+
+    else:
+        form = EmailPostForm()
+    
+    return render(
+        request, 
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
+    ) 
+
 
 def post_list(request):
     all_posts = Post.published.all()
