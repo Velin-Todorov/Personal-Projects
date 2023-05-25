d9a3805c318940bb70d80bb00228d9ab8ccaa5c2
Velin-Todorov <velintodorov@yahoo.com>
2023-05-25 16:51:30 +0200

    Implemented simple paginations

--- Projects with Django/blog_application/blog/views.py
+++ Projects with Django/blog_application/blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.published.all()
+
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+
+    posts = paginator.page(page_number)
+
     return render(
         request,
         'blog/post/list.html',
