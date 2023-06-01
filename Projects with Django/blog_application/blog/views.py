e71734672cd526b7fd60ec9f6ba2cb24b483283d
Velin-Todorov <velintodorov@yahoo.com>
2023-06-01 22:55:20 +0200

    implemented Similar posts

--- Projects with Django/blog_application/blog/views.py
+++ Projects with Django/blog_application/blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from dotenv import load_dotenv
 from taggit.models import Tag
+from django.db.models import Count
 import os
 
 load_dotenv()
@@ -96,13 +97,18 @@ def post_detail(request, year, month, day, post):
 
     form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
