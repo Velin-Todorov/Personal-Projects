58aa49a893cc1490a60db6ab99680f14f55e8f16
Velin-Todorov <velintodorov@yahoo.com>
2023-06-09 20:47:46 +0200

    Defined user model

--- Projects with Django/blog_application/users/models.py
+++ Projects with Django/blog_application/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class User(models.Model):
+    
+    class Verified(models.TextChoices):
+        YES = 'Y', 'Yes'
+        NO = 'N', 'No'
+
+    class Meta:
+        db_table = 'users'
+
+    username = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Verified.choices,
+        default=Verified.NO
+    )
+    followers = models.BigIntegerField(default=0)
