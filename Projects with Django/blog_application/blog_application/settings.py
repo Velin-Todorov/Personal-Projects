16c9ec5f2ecf36ffb2c11d44d69ce27e4faa3824
Velin-Todorov <velintodorov@yahoo.com>
2023-06-09 20:15:38 +0200

    inital commit on users

--- Projects with Django/blog_application/blog_application/settings.py
+++ Projects with Django/blog_application/blog_application/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'blog.apps.UsersConfig',
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
