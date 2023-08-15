1b3d7fff54dbf8eb51e09064326bcff976c19bfc
Velin Todorov <velintodorov@yahoo.com>
2023-08-15 20:09:35 +0200

    changes

--- Projects with Django/simple_social_website/account/urls.py
+++ Projects with Django/simple_social_website/account/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     ),
 
     path('', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register')
-    
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
 ]
