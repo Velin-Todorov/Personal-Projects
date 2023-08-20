936ccfad45485f768e3f5d4350c24150079beb29
Velin Todorov <velintodorov@yahoo.com>
2023-08-20 13:18:43 +0200

    started wokring on bookrmarking functionality

--- Projects with Django/simple_social_website/images/models.py
+++ Projects with Django/simple_social_website/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 # Create your models here.
 
 class Image(models.Model):
@@ -14,13 +15,24 @@ class Image(models.Model):
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='images_liked',
+        blank = True
+    )
     created = models.DateField(auto_now_add=True)
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
         ]
-        ordering = ['-ordering']
+        ordering = ['-created']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
--- Projects with Django/simple_social_website/images/views.py
+++ Projects with Django/simple_social_website/images/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreationForm
 
 # Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreationForm(data=request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+
+            messages.success(request, 'Image added successfully')
+
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreationForm(data=request.GET)
+    
+    return render(
+        request,
+        'images/image/create.html',
+        {
+            'section': 'images',
+            'form': form
+        }
+    )
+        
