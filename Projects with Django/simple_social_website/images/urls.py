d13c7262250c70b9542371d2e7184de7d0d72762
Velin-Todorov <96660814+Velin-Todorov@users.noreply.github.com>
2023-09-16 18:13:38 +0200

    Delete Projects with Django/simple_social_website directory

--- Projects with Django/simple_social_website/images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='images'
-
-urlpatterns = [
-    path('create/', views.image_create, name='create'),
-    path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
-]
