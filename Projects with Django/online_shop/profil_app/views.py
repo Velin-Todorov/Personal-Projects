30e08b0d6cb55767946f50d5694b56dc1b562b17
Velin Todorov <velintodorov@yahoo.com>
2023-09-24 00:04:20 +0200

    added profile app

---
+++ Projects with Django/online_shop/profil_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+class CreateProfile:
+    pass
+
+class DeleteProfile:
+    pass
+
+
+class EditProfile:
+    pass
+
+
+class UpdateProfile:
+    pass
