91e87b100060cce572c08cf5157d4b11cbf831ce
Velin-Todorov <velintodorov@yahoo.com>
2023-11-18 17:56:57 +0100

    added login and register paths

--- Projects with Django/online_shop/auth_views/urls.py
+++ Projects with Django/online_shop/auth_views/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.LoginView.as_view(), name='login_view')
+    path('login', views.LoginView.as_view(), name='login_view'),
+    path('register', views.RegisterView.as_view(), name='register_view')
 ]
